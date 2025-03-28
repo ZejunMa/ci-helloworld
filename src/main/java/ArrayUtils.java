766ada123466cb3d5416131429f89eaf0260b268
ZejunMa <zejunma9@gmail.com>
2025-03-27 23:02:54 -0400

    added function

--- src/main/java/ArrayUtils.java
+++ src/main/java/ArrayUtils.java
@@ -1,5 +1,12 @@
 public class ArrayUtils {
 
+    public static int countOf(int[] x, int target) {
+        int count = 0;
+        for (int a: x) {
+            count = a == target ? count + 1 : count;
+        }
+        return count;
+    }
     /**
      * Find last index of element
      *
